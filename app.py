572cb266ba85859771e3ceda8c829664b7b708fd
vyshnavtp <vyshnavtp27@gmail.com>
2020-09-30 19:54:44 +0530

    myfirst

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '718a4ee2b889319dca57cf8b929cac22'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from carp import routes
+
+
